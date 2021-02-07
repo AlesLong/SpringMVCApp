e37208583acf9474ced94bbdbb7d6033484ca4c4
AlesLong <yevbes@gmail.com>
2021-02-07 02:24:03 +0200

    first commit

---
+++ src/main/java/SpringMVCApp/config/DAO/PersonDAO.java
@@ -0,0 +1,29 @@
+package SpringMVCApp.config.DAO;
+
+
+import SpringMVCApp.config.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(1, "Tom"));
+        people.add(new Person(2, "Cat"));
+        people.add(new Person(3, "Spring"));
+        people.add(new Person(4, "Eugene"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
---
+++ src/main/java/SpringMVCApp/config/controllers/PeopleController.java
@@ -0,0 +1,38 @@
+package SpringMVCApp.config.controllers;
+
+import SpringMVCApp.config.DAO.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+
+    private final PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+
+        //get all people from DAO and send to view
+
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        //Get 1 person by id from DAO and send to view
+
+        return "people/show";
+    }
+}
